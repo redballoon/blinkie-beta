9d9924f8597a74b0e94b2d09f0a564bf45042cb9
Fred <fred@miniDev-2.local>
2016-04-20 23:15:18 -0400

    testing

--- index.js
+++ index.js
@@ -11,17 +11,37 @@ process.on( 'SIGINT', function() {
 
 leds.connect(160);
 leds.clear();
-leds.fill(0xFF, 255, 0x00);
+leds.fill(0, 0, 255);
 
 setTimeout(function () {
 	console.log('timeout');
-	leds.setRGB(0, '#FF0000');
-	leds.setRGB(159, '#FF0000');
-	leds.update();
-}, 5000);
+	//leds.setRGB(0, '#FF0000');
+	//leds.setRGB(0, [255, 0, 0]);
+	//leds.setRGB(159, '#FF0000');
+	//leds.update();
+	
+	var index = 1;
+	var colors = leds.getRGBArray(255, 0, 0);
+	var blank = leds.getRGBArray(0, 0, 0);
+	var total = leds.getChannelCount();
+	var colorBuffer = new Buffer(total);
+	for (var i = 0; i < total; i += 3){
+		if (index * 3 === i) {
+			colorBuffer[i + 0] = colors[0];
+			colorBuffer[i + 1] = colors[1];
+			colorBuffer[i + 2] = colors[2];
+		} else {
+			colorBuffer[i + 0] = blank[0];
+			colorBuffer[i + 1] = blank[1];
+			colorBuffer[i + 2] = blank[2];
+		}
+		
+	}
+	leds.sendRgbBuffer(colorBuffer);
+}, 2000);
 
 
 setTimeout(function () {
 	leds.clear(); 
 	leds.disconnect();
-}, 10000);
+}, 8000);
